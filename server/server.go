0a8baf6e4f3d5635767cfa5684bf864e8c97e663
sagar.dash290 <sagar.dash290@gmail.com>
2021-04-28 09:42:53 +0600

    change file structure

--- server/server.go
+++ server/server.go
@@ -5,8 +5,9 @@ import (
 	"net/http"
 )
 
+// go run server.go
 func main() {
-	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../assets")))
+	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../public")))
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 		return
